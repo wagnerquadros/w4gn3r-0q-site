1fd0a0398111541ba7f314c0e7b5795705e656db
wagnerquadros <woquadros@gmail.com>
2026-07-26 15:33:20 -0300

    feat: adiciona estrutura inicial do projeto Angular

---
+++ src/app/app.ts
@@ -0,0 +1,11 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('w4gn3r-0q-site');
+}
